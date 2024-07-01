09836496bc0c9b40ca61f7cd92538e2c09f1ad02
Vikash Kumar <vkumar@agelixconsulting.com>
2024-07-02 00:04:02 +0530

    gile structure changed

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import( 
+	"os"
+	"github.com/gin-gonic/gin"
+	routes "golang-RESTAURANTS_MANAGEMENT_IN_GO/routes"
+	// "golang-RESTAURANTS_MANAGEMENT_IN_GO/database"
+	// "golang-RESTAURANTS_MANAGEMENT_IN_GO/middleware"
+	// "go.mongodb.org/mongo-driver/mongo"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port= "8000";
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	routes.AuthRoutes(router)
+	
+}	
